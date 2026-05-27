8e1f3f7bbdf64e04c687127dec5dded1f4a0e6d4
javed.tamboli <javed.tamboli33@gmail.com>
2026-05-27 10:43:07 +0530

    Project structure done

---
+++ src/main/java/com/example/event_ledger_api/EventLedgerApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.event_ledger_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventLedgerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventLedgerApiApplication.class, args);
+	}
+
+}
